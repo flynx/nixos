f898453e0e6f3d3bb1ab65c5d8a14f9e88330646
Alex A. Naanou <alex.nanou@gmail.com>
2024-03-30 18:48:48 +0300

    added intel video drivers...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,23 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+
+
+  # intel video drivers...
+  # XXX move to hardware...
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
   
 
   # Bootloader.
